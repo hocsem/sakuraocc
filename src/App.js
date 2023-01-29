cc5d1e4c70fd1a9e6fc3dcf626c8146f7330d7e8
hocsem <120021690+hocsem@users.noreply.github.com>
2023-01-29 17:21:32 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import Componente from './Componente';
+function App() {
+  return (
+    <div className="App">
+      
+    <p>hola mundo</p>  
+  </div>
+
+   
+   
+
+
+
+   
+
+  
+<Componente/>
+        
+   
+  );
+}
+
+export default App;
